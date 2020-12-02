9e0719b32d048de2584d1d35b4dca4130324598c
sp-devi <spneuro01@gmail.com>
2020-12-02 22:30:10 +0900

    Made date parameters dynamic

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const puppeteer = require('puppeteer');
 const url = 'https://www.net.city.nagoya.jp/cgi-bin/sp04001';
 
 async function run () {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
     const page = await browser.newPage();
     await page.goto(url);
+
+    // current date
+    let nodeDate = new Date();
+    // adjust 0 before single digit date
+    let date = ("0" + nodeDate.getDate()).slice(-2);
+    // current month
+    let month = ("0" + (nodeDate.getMonth() + 1)).slice(-2);
+
     // Select boxes
     await page.select('select[name="syumoku"]', '023');
-    await page.select('select[name="month"]', '12');
-    await page.select('select[name="day"]', '18');
+    await page.select('select[name="month"]', month);
+    await page.select('select[name="day"]', date);
     await page.select('select[name="kyoyo1"]', '07');
     await page.select('select[name="kyoyo2"]', '07');
     await page.select('select[name="chiiki"]', '20');
@@ -16,6 +29,7 @@ async function run () {
     await page.click('input[type="submit"][name="button"]');
 
     await page.waitFor(5000);
+
     const list = await page.evaluate(() => {
         let data = [];
         const list = document.querySelectorAll('.RESOUTPUT2');
@@ -26,7 +40,6 @@ async function run () {
                 'link': a.querySelector('a').href,
                 'date': a.nextElementSibling.innerText,
                 'schedule': schedule.innerText
-
             });
         }
         return data;
--- puppet.js
+++ puppet.js
@@ -1,39 +1,7 @@
-const puppeteer = require('puppeteer');
-const url = 'https://www.net.city.nagoya.jp/cgi-bin/sp04001';
+let date = new Date();
 
-async function run () {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    // Select boxes
-    await page.select('select[name="syumoku"]', '023');
-    await page.select('select[name="month"]', '12');
-    await page.select('select[name="day"]', '18');
-    await page.select('select[name="kyoyo1"]', '07');
-    await page.select('select[name="kyoyo2"]', '07');
-    await page.select('select[name="chiiki"]', '20');
-    await page.click('input[name="joken"][value="2"]');
-    await page.click('input[type="submit"][name="button"]');
+let date2 = ("0" + date.getDate()).slice(-2);
 
-    await page.waitFor(5000);
-    const list = await page.evaluate(() => {
-        let data = [];
-        const list = document.querySelectorAll('.RESOUTPUT2');
-        for (const a of list) {
-            var schedule = a.nextElementSibling.nextElementSibling;
-            data.push({
-                'name': a.innerText,
-                'link': a.querySelector('a').href,
-                'date': a.nextElementSibling.innerText,
-                'schedule': schedule.innerText
+console.log(date.getDate());
 
-            });
-        }
-        return data;
-    })
-    console.log(list);
-
-    //await page.screenshot({path: 'screenshot.png'});
-    browser.close();
-}
-run();
+console.log(date2);
