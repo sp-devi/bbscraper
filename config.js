de2a92ed4a458d6b9fe5fc73183c8b87965bb552
sp-devi <spneuro01@gmail.com>
2020-12-08 22:35:45 +0900

    Added aws config file

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'aws': {
+        'key': 'MY ACCESS KEY',
+        'secret': 'ACCESS SECRET',
+        'ses': {
+            'from': {
+                'default': 'my-email'
+            },
+            'region': 'MY REGION'
+        }
+    }
+};
