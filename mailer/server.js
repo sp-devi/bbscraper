7aec7ea43fca2b0afe9625da63fa8af6f98ad74f
sp-devi <spneuro01@gmail.com>
2020-12-08 23:15:53 +0900

    Added endpoints for server js

--- mailer/server.js
+++ mailer/server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+const mailClient = require('./mail-client');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    mailClient.sendEmail("ianemail@gmail.com", "Hey welcome", "Body of email");
+    res.send('Email is sent');
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+})
