9fba72f8be381f7b49cf8455d718fdc0d3b1b818
sp-devi <spneuro01@gmail.com>
2020-12-08 22:20:33 +0900

    Added AWS SES config and mail sending logic

--- process.js
+++ process.js
@@ -1,2 +1,45 @@
+const AWS = require('aws-sdk');
+
 // For Email sending
 const EMAIL = "apd@gmail.com"
+
+
+AWS.config.update({
+    accessKeyId: config.aws.key,
+    secretAccessKey: config.aws.secret,
+    region: config.aws.ses.region
+});
+
+const ses = new AWS.SES({
+    apiVerson: '2010-12-01'
+});
+
+const sendEmail = (to, subject, message, from) => {
+    const params = {
+        Destination: {
+            ToAddress: [to]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: message
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: subject
+            }
+        },
+        ReturnPath: from ? from : config.aws.sets.from.defaul,
+        Source: from ? from : config.aws.ses.from.defaul
+    };
+
+    ses.sendEmail(params, (err, data) => {
+        if (err) {
+            return console.log(err, err.stack);
+        } else {
+            console.log("Email send : ", data);
+        }
+    })
+}
