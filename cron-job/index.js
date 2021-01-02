00f66d0c757c14fb4bcd0babb92532dd2257b253
sp-devi <spneuro01@gmail.com>
2021-01-02 23:19:18 +0900

    Added schedule for cron tab

--- cron-job/index.js
+++ cron-job/index.js
@@ -7,4 +7,9 @@ cron.schedule('* * * * *', () => {
     console.log('Running a task every 10 minutes');
 })
 
+// Schedule runs at 8 AM every day? Verify cron tab syntax
+cron.schedule('00 08 * * *', () => {
+    // Add call for controller to run the scraper
+});
+
 app.listen(3000);
