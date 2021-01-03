b1713f752498458f84ff5543bd9247dcf0a3da84
sp-devi <spneuro01@gmail.com>
2021-01-03 21:31:07 +0900

    Implemented the scraper controller API's

--- controller.js
+++ controller.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const scraper = require('./index.js');
+
+const app = express();
+
+// Change to appropriate Endpoint API
+app.get('/bbscraper', (req, res) => {
+    scraper.scrape();
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+})
