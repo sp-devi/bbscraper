911f11ff32843111efbd598b2df789df3d2ab820
sp-devi <spneuro01@gmail.com>
2021-01-03 21:32:34 +0900

    Added service calls for bbscraper scrape function

--- cron-job/index.js
+++ cron-job/index.js
@@ -10,6 +10,11 @@ cron.schedule('* * * * *', () => {
 // Schedule runs at 8 AM every day? Verify cron tab syntax
 cron.schedule('00 08 * * *', () => {
     // Add call for controller to run the scraper
+    request('/bbscraper', function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+          console.log(body)
+       }
+    })
 });
 
 app.listen(3000);
