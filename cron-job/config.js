88c054b3f90985e0446935dd18eb314460092432
spdevi <spneuro01@gmail.com>
2021-03-22 21:37:06 +0900

    Change cron job to every minute

--- cron-job/config.js
+++ cron-job/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'cron_field': {
         'second': '0',
-        'minute': '*/3',
+        'minute': '*/1',
         'hour': '8-23',
         'dayOfMonth': '*',
         'month': '*',
